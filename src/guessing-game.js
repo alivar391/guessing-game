16541fdce9279f6e5e088b918e3bc6978dbe20c9
alivar391 <patriot391@mail.ru>
2023-02-24 15:54:59 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
     constructor() {}
-
+    candidate = 0;
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.candidate = Math.round((this.max - this.min) / 2 + this.min);
     }
 
     lower() {
-
+        return this.max = this.candidate;
     }
 
     greater() {
-
+        return this.min = this.candidate;
     }
 }
 
